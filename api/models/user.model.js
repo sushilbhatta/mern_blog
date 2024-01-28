d4c5b8e2758d4a5680b5744b6591e3da7438fc7f
sushilbhatta <sushilbhatta58@gmail.com>
2024-01-28 13:37:54 +0545

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamp: true } //2n argument for timestamps
+);
+const User = mongoose.model("User", userSchema);
+export default User;
